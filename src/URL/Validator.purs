c0820d20b52ac110c65ded1ebac499453460ff1c
Brandon Elam Barker <beb82@cornell.edu>
2019-04-27 18:47:44 +0000

    very minor

--- src/URL/Validator.purs
+++ src/URL/Validator.purs
@@ -16,15 +16,13 @@ foreign import _validateURL :: Boolean -> String -> String
 -- | Returns `true` if a valid URL is passed in, `false` otherwise.
 validateURL :: String -> Boolean
 validateURL urlIn =_checkHrefStatus hrefStatus
-  where
-    hrefStatus = _validateURL false urlIn
+  where hrefStatus = _validateURL false urlIn
 
 -- | Returns true if a valid URL is passed in, `false` otherwise.
 -- | Also returns `false` if the URL contains a username or password.
 validatePublicURL :: String -> Boolean
 validatePublicURL urlIn = _checkHrefStatus hrefStatus
-  where
-    hrefStatus = _validateURL true urlIn
+  where hrefStatus = _validateURL true urlIn
 
 _checkHrefStatus :: String -> Boolean
 _checkHrefStatus "SUCCESS" = true
@@ -34,16 +32,14 @@ _checkHrefStatus _ = false
 -- | Otherwise, returns an error message string.
 parseURL :: String -> Either String URL
 parseURL urlIn = _statusToURL urlIn hrefStatus
-  where
-    hrefStatus = _validateURL false urlIn
+  where hrefStatus = _validateURL false urlIn
 
 -- | Returns a URL if it is determiend the input string is a URL
 -- | and that the URL contains no username or password.
 -- | Otherwise, returns an error message string.
 parsePublicURL :: String -> Either String URL
 parsePublicURL urlIn = _statusToURL urlIn hrefStatus
-  where
-    hrefStatus = _validateURL true urlIn
+  where hrefStatus = _validateURL true urlIn
 
 _statusToURL :: String -> String -> Either String URL
 _statusToURL urlIn status = case _checkHrefStatus status of
