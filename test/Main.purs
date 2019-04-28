64a2621f3c0e54cda2356b06502909486c20bf9b
bbarker <beb82@cornell.edu>
2019-04-28 18:14:57 +0000

    a few minor tests

--- test/Main.purs
+++ test/Main.purs
@@ -11,16 +11,26 @@ import URL.Validator
 
 goodUrl1 :: String
 goodUrl1 = "https://www.example.com:80/page?q=foobar#section"
+goodUrl2 :: String
+goodUrl2 = "https://www.example.com:80/pa+ge?q=foobar#section"
+goodUrl3 :: String
+goodUrl3 = "https://www.example.com:80/pa%20ge?q=foobar#section"
 
 badUrl1 :: String
 badUrl1 = "httqs://www.example.com:80/page?q=foobar#section"
+badUrl2 :: String
+badUrl2 = "https://www.exa mple.com:80/page?q=foobar#section"
 
 main :: Effect Unit
 main = runTest do
   suite "Public URLs that should validate" do
     publicUrlTestPositive goodUrl1
+    publicUrlTestPositive goodUrl2
+    publicUrlTestPositive goodUrl3
+
   suite "Public URLs that should NOT validate" do
     publicUrlTestNegative badUrl1
+    publicUrlTestNegative badUrl2
 
 publicUrlTestPositive :: String -> TestSuite
 publicUrlTestPositive url = test url do
