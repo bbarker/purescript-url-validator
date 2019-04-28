6c82993a18bddb79e8285c88f7c1f52e75d0548d
Brandon Elam Barker <beb82@cornell.edu>
2019-04-28 12:32:02 +0000

    added a negative test

--- test/Main.purs
+++ test/Main.purs
@@ -2,22 +2,32 @@ module Test.Main where
 
 import Prelude
 
-import Data.Either                       (isRight)
+import Data.Either                       (isLeft, isRight)
 import Effect                            (Effect)
 import Test.Unit                         (TestSuite, suite, test)
 import Test.Unit.Main                    (runTest)
 import Test.Unit.Assert                  as Assert
 import URL.Validator
 
-url1 :: String
-url1 = "https://www.example.com:80/page?q=foobar#section"
+goodUrl1 :: String
+goodUrl1 = "https://www.example.com:80/page?q=foobar#section"
+
+badUrl1 :: String
+badUrl1 = "httqs://www.example.com:80/page?q=foobar#section"
 
 main :: Effect Unit
 main = runTest do
   suite "Public URLs that should validate" do
-    publicUrlTestPositive url1
+    publicUrlTestPositive goodUrl1
+  suite "Public URLs that should NOT validate" do
+    publicUrlTestNegative badUrl1
 
 publicUrlTestPositive :: String -> TestSuite
 publicUrlTestPositive url = test url do
   Assert.assert (url <> " should validate") $ validatePublicURL url
   Assert.assert (url <> " should return Right") $ isRight $ parsePublicURL url
+
+publicUrlTestNegative :: String -> TestSuite
+publicUrlTestNegative url = test url do
+  Assert.assertFalse (url <> " should NOT validate") $ validatePublicURL url
+  Assert.assert (url <> " should return Left") $ isLeft $ parsePublicURL url
